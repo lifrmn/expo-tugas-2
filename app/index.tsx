e5b395b1568d18be827a5c61ce6d4b2f35229233
ALIF RHMNS <139601351+lifrmn@users.noreply.github.com>
2025-07-27 02:26:32 +0800

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,81 +1,28 @@
-import { ScrollView, Text } from "react-native";
-
-const data = [
-  { name: "BASO HAMZAH", 
-    nim: "105841106922",
-    font: "B612-Italic", 
-    size: 12 },
-  {
-    name: "Muh. Akbar Haeruddin",
-    nim: "105841104622",
-    font: "Fenix-Regular",
-    size: 14,
-  },
-  {
-    name:"Dia Rahmatillah",
-    nim: "105841104822",
-    font: "Gupter-Medium",
-    size: 16,
-  },
-  {
-    name: "Dia Rahmatillah",
-    nim: "105841104822",
-    font: "Marvel-Bold",
-    size: 18,
-  },
-  {
-    name: "Juliani",
-    nim: "105841104922",
-    font: "Sniglet-ExtraBold",
-    size: 20,
-  },
-  {
-    name: "Azzah Aulia Syarif",
-    nim: "105841105022",
-    font: "Alkatra-VariableFont",
-    size: 24,
-  },
-  {
-    name: "Syauqiyah Mujahidah Yahya",
-    nim: "105841105122",
-    font: "ExpletusSans-VariableFont",
-    size: 26,
-  },
-  {
-    name: "Mar'atul Azizah",
-    nim: "105841105222",
-    font: "HostGrotesk-VariableFont",
-    size: 28,
-  },
-  {
-    name: "Fikrah Lejahtegis",
-    nim: "105841105322",
-    font: "InclusiveSans-VariableFont",
-    size: 30,
-  },
-  {
-    name: "Alya Anandha",
-    nim: "105841105422",
-    font: "PlaywriteAUSA-VariableFont",
-    size: 36,
-  },
-];
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Index() {
   return (
-    <ScrollView contentContainerStyle={{ padding: 10 }}>
-      {data.map((item, index) => (
-        <Text
-          key={index}
-          style={{
-            fontFamily: item.font,
-            fontSize: item.size,
-            marginBottom: 12,
-          }}
-        >
-          {item.name + "\n" + item.nim}
-        </Text>
-      ))}
+    <ScrollView contentContainerStyle={{ padding: 20, flexGrow: 1 }}>
+      <Text style={{ fontSize: 20, marginBottom: 10 }}>Daftar 10 Ikon:</Text>
+
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+        <AntDesign name="stepforward" size={30} color="black" />
+        <AntDesign name="stepbackward" size={30} color="black" />
+        <Entypo name="500px" size={30} color="black" />
+        <Entypo name="aircraft" size={30} color="black" />
+        <EvilIcons name="bell" size={30} color="black" />
+        <EvilIcons name="calendar" size={30} color="black" />
+        <Feather name="activity" size={30} color="black" />
+        <Feather name="airplay" size={30} color="black" />
+        <FontAwesome name="heart" size={30} color="black" />
+        <FontAwesome name="star" size={30} color="black" />
+      </View>
     </ScrollView>
   );
 }
